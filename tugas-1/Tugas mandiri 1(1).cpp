10eda1e1641473b22b90972712f464c547704b82
unknown <in130204@gmail.com>
2023-10-14 15:22:58 +0700

    first commit

---
+++ tugas-1/Tugas mandiri 1(1).cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    
+    char nim[] = "112233";
+    char nama[] = "Intan Nuraeni";
+    char jurusan[] = "Teknik Informatika"; 
+
+    
+    printf("NIM: %s\n", nim);
+    printf("NAMA: %s\n", nama);
+    printf("JURUSAN: %s\n", jurusan);
+
+    return 0;
+}
---
+++ tugas-2/Tugas mandiri 2(1).cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	
+	int alas = 8;
+	int tinggi = 5;
+	printf("Diketahui sebuah segitiga memiliki panjang alas =8cm dan tinggi =5cm hitunglah luas segitiga tersebut");
+	printf("\njawabanya adalah %d cm" ,(0,5*alas*tinggi));
+	
+	return 0;
+}
